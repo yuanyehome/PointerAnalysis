1cfc2acb5e2f68b925a803b771708bc9f64f2bec
yuanyehome <2252706531@qq.com>
2019-11-28 02:15:22 +0800

    solve conflict

--- code/test/ycytest1.java
+++ code/test/ycytest1.java
@@ -7,14 +7,10 @@ public class Hello {
 
   public static A internal(A k) {
     BenchmarkN.alloc(4);
-<<<<<<< HEAD
     A d = new A();
     k = d;
     BenchmarkN.test(4, d);
     return d;
-=======
-    A a1 = new A();
->>>>>>> a9d665f1d3592fe145cd19eb59d721a8095c6ff0
   }
 
   public static void main(String[] args) {
@@ -28,14 +24,8 @@ public class Hello {
     if (args.length > 1) a = b;
     if (args.length > 2) b = c;
     BenchmarkN.test(2, a);
-<<<<<<< HEAD
     c = internal(b);
     BenchmarkN.test(3, c);
     BenchmarkN.test(4, b);
-=======
-    BenchmarkN.test(3, b);
-    BenchmarkN.test(4, c);
-    if (args.length > 3) c = a;
->>>>>>> a9d665f1d3592fe145cd19eb59d721a8095c6ff0
   }
 }
